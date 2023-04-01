1015ed4796df1883e94cdbf640e779ca6d45550c
Juan <portillafelipe510@gmail.com>
2023-03-31 21:41:41 -0500

    introduccion en js

---
+++ variables.js
@@ -0,0 +1,39 @@
+console.log("hola mundo");
+
+var nombre = "juan";
+console.log(nombre);
+let  apellido = prompt("ingrese su apellido");
+
+var nombre = "felipe";
+console.log (nombre);
+
+let edad = Number(prompt("ingrese su edad"));
+console.log(edad);
+
+console.log(edad);
+
+var acesso = true;
+var persona = ["juan ", "felipe", " sebas" ]
+var numero = [1,2,3,4,5,]
+var detodito = [1,"hola" , [0],{mar:"BMW"}];
+console.log(typeof detodito );
+detodito[1]= "adios"
+console.log(detodito [1]);
+var carro = {marca:"BMW",
+fechaFabicacion : 1965,
+color: "rosadito"
+};
+console.log(carro.fechaFabicacion);
+console.log(carro["color"]);
+
+
+function saludar(){
+    console.log("hola mundo ");
+};
+
+function suma(numeroa, numerob){
+    return numeroa+numerob
+
+};
+console.log(suma(18,12;));
+
