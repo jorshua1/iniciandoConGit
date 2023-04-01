e822d04240bb8a037fce8fb422d56910a1bb92ed
DavidS <ds0299646@gmail.com>
2023-03-31 21:41:08 -0500

    introduccion JS

---
+++ variables.js
@@ -0,0 +1,15 @@
+
+var nombre= "David";
+console.log(nombre);
+
+let apellido = prompt ("ingrese su apellido");
+
+let edad = Number(prompt(18));
+
+console.log(edad*10);
+
+var acceso = true;
+var personas = ["David","javier","sebas","benjamin","maritza"];
+var numeros = [1,2,3,4,5];
+var chesse_tris =[1,"david", [0], {marca:"bmw"}];
+console.log(typeof(chesse_tris));
