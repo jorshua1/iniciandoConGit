95915d844c9393cd9f8c0443e805d06c84a75144
YawingCat <mancocat777@gmail.com>
2023-03-31 21:41:25 -0500

    introduccion de java script

---
+++ variables.js
@@ -0,0 +1,41 @@
+console.log("hola mundo");
+
+var nombre = "Danilo"
+   console.log(nombre);
+
+
+
+let edad = Number(prompt ("ingrese su edad"));
+
+console.log(edad<0);
+console.log(edad*10);
+
+var access = true;
+var peoples = ["ramiro","danilo","omar"];
+var numbers = ["1","2", "3", "4", ];
+detodito = [1,"hola",[0], {marca: "bmw"}];
+console.log(typeof detodito);
+detodito[1]="adios"
+console.log(detodito[0]);
+
+var carro = {marca: "BMW",
+fechaFabricacion:1950, 
+color: "rojo"
+};
+
+console.log(carro.color);
+
+function saludar() {
+    console.log("hola mundo");
+    
+
+
+}
+
+
+function suma(numeroA,numeroB){
+    return numeroA + numeroB;
+
+    
+}
+console.log(suma(18,12));
