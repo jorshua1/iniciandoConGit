78533278e6e2bb115ed8fd7852fbbd2f502eba8f
jhon <jhonhenry9611@hotmail.com>
2023-03-31 21:43:05 -0500

    introduccion a javascript

---
+++ variables.js
@@ -0,0 +1,14 @@
+console.log("hola mundo");
+var nombre ="jorshua"
+let apellido=prompt("ingrese su apellido");
+let edad=Number(prompt("ingrese su edad"));
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(edad*20);
+var acceso = true;
+var personas=["paola","jhon","felipe"];
+var numero=[1,2,3,4,5];
+var detodito=[1,"hola",[0],{marca:"bmw"}];
+console.log(typeof detodito);
+
